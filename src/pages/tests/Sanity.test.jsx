bc63cbd729cb798347a00ccde35122fa68661ce9
Christian <christian_rose@live.ca>
2025-08-07 21:24:19 -0230

    Polish

--- src/pages/tests/Sanity.test.jsx
+++
@@ -1,9 +0,0 @@
-import { describe, it, expect } from 'vitest'
-
-describe('Sanity Test', () => {
-  it('can access document', () => {
-    const div = document.createElement('div')
-    div.textContent = 'hello'
-    expect(div.textContent).toBe('hello')
-  })
-})
